0c7eb68d1345022371c19f4904a177c0be3dfa24
JaceJung-dev <jacejung.dev@gmail.com>
2025-11-10 15:20:15 +0900

    💄 Update lualine for showing Lazy plugin update status

--- .config/nvim/lua/jwoong/plugins/lualine.lua
+++ .config/nvim/lua/jwoong/plugins/lualine.lua
@@ -52,6 +52,18 @@ return {
 			options = {
 				theme = my_lualine_theme,
 			},
+			sections = {
+				lualine_x = {
+					{
+						lazy_status.updates,
+						cond = lazy_status.has_updates,
+						color = { fg = "#ff9e64" },
+					},
+					{ "encoding" },
+					{ "fileformat" },
+					{ "filetype" },
+				},
+			},
 		})
 	end,
 }
