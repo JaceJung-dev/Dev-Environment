3b9604838f6ebbc6c851bf43538946af2f44a691
JaceJung-dev <jacejung.dev@gmail.com>
2025-11-10 18:04:16 +0900

    ✅ Add mason-lspconfig
    - Added mason-lspconfig in `mason.lua`, and placed `mason` in `dependencies` to ensure it runs first
    - Also added `nvim-lspconfig` to `dependencies`

--- .config/nvim/lua/jwoong/plugins/lsp/mason.lua
+++ .config/nvim/lua/jwoong/plugins/lsp/mason.lua
@@ -1,12 +1,32 @@
 return {
-	"mason-org/mason.nvim",
-	opts = {
-		ui = {
-			icons = {
-				package_installed = "✓",
-				package_pending = "➜",
-				package_uninstalled = "✗",
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			-- list of servers of mason to install
+			ensure_installed = {
+				"lua_ls", -- lua
+				"pyright", -- python
+				"clangd", -- C/C++
+				"ts_ls", -- JavaScript/TypeScript
+				"html", -- HTML
+				"cssls", -- CSS
+				"tailwindcss", -- Tailwind CSS
 			},
+		},
+		dependencies = {
+			{
+				"mason-org/mason.nvim",
+				opts = {
+					ui = {
+						icons = {
+							package_installed = "✓",
+							package_pending = "➜",
+							package_uninstalled = "✗",
+						},
+					},
+				},
+			},
+			"neovim/nvim-lspconfig",
 		},
 	},
 }
