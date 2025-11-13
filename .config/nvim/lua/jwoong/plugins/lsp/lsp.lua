e5fa69d239c9bcf975ab6624cd65511972f73ecc
JaceJung-dev <jacejung.dev@gmail.com>
2025-11-13 20:28:28 +0900

    ✅ ) Update auto-detect Python venv

--- .config/nvim/lua/jwoong/plugins/lsp/lsp.lua
+++ .config/nvim/lua/jwoong/plugins/lsp/lsp.lua
@@ -12,6 +12,43 @@ return {
 		-- used to enable autocompletion (assign to every lsp server config)
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
+		-- Retrieve Python venv
+		local function get_python_path()
+			local venv_paths = { ".venv", "venv" }
+			local cwd = vim.fn.getcwd()
+
+			for _, venv in ipairs(venv_paths) do
+				local venv_python = cwd .. "/" .. venv .. "/bin/python"
+				if vim.fn.filereadable(venv_python) == 1 then
+					return venv_python
+				end
+			end
+
+			return vim.fn.exepath("python3") or vim.fn.expath("python") or "python"
+		end
+
+		-- pyright setup
+		vim.lsp.config.pyright = {
+			cmd = { "pyright-langserver", "--stdio" },
+			filetypes = { "python" },
+			root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", ".git" },
+			capabilities = capabilities,
+			settings = {
+				python = {
+					pythonPath = get_python_path(),
+					analysis = {
+						autoSearchPaths = true,
+						useLibraryCodeForTypes = true,
+						diagnosticMode = "workspace",
+						typeCheckingMode = "basic",
+						diagnosticSeverityOverrides = {
+							reportAttributeAccessIssue = "warning",
+						},
+					},
+				},
+			},
+		}
+
 		vim.lsp.config("*", {
 			capabilities = capabilities,
 		})
