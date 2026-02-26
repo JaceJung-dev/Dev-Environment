0dd9cfc631038c3408244688d1e6ca0ecaa180b4
JaceJung-dev <jacejung.dev@gmail.com>
2026-02-26 18:56:30 +0900

    ♻️ Rewrite ltex LSP config to Neovim 0.11+ after/lsp format

--- .config/nvim/after/lsp/ltex.lua
+++ .config/nvim/after/lsp/ltex.lua
@@ -1,20 +1,10 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        ltex = {
-          filetypes = { "markdown", "text", "gitcommit" },
-
-          settings = {
-            ltex = {
-              language = "en-US", -- 기본 언어는 영어
-              additionalRules = {
-                motherTongue = "ko-KR", -- 한국어도 같이 고려하도록
-              },
-            },
-          },
-        },
+  filetypes = { "markdown", "text" },
+  settings = {
+    ltex = {
+      language = "en-US",
+      additionalRules = {
+        motherTongue = "ko-KR",
       },
     },
   },
