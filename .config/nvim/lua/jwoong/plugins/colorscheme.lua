85b850406c22df0b3d96c0dcc1392081c24c1698
JaceJung-dev <jacejung.dev@gmail.com>
2026-03-03 13:22:06 +0900

    ✨ Enhance catppuccin theme config for nvim and wezterm

--- .config/nvim/lua/jwoong/plugins/colorscheme.lua
+++ .config/nvim/lua/jwoong/plugins/colorscheme.lua
@@ -1,11 +1,34 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		require("catppuccin").setup({
-			flavour = "macchiato",
-		})
-		vim.cmd("colorscheme catppuccin")
-	end,
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      flavour = "macchiato",
+
+      dim_inactive = {
+        enabled = true,
+        shade = "dark",
+        percentage = 0.10,
+      },
+
+      transparent_background = false,
+
+      integrations = {
+        treesitter = true,
+        which_key = true,
+        mason = true,
+        indent_blankline = { enabled = true },
+
+        native_lsp = {
+          enabled = true,
+          semantic_tokens = true,
+        },
+
+        render_markdown = true,
+      },
+    })
+
+    vim.cmd.colorscheme("catppuccin")
+  end,
 }
